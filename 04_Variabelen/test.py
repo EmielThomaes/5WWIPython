a791d40db5a8aff9ba3c14dba661f88d53e774ba
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-09-20 11:12:41 +0200

    Patroontje

---
+++ 04_Variabelen/test.py
@@ -0,0 +1,19 @@
+# wisselen van waarde
+# x, y = 2, 'hallo'
+# x, y = y, x
+# print(x, y)
+# wordt altijd per regel uitgevoerd: x = y; y = x -> zelfde waarde
+# input is altijd een string
+
+# programma die de discriminant berekent
+# invoer
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+
+# berekening
+discriminant = (b**2) - (4 * a * c)
+
+# uitvoer
+print(discriminant)
+#--------------------------------------
