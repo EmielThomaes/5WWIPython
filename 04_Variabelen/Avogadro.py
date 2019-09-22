e7dbae227c78498ebb0a8e3d5bb31301b78e3c1c
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-09-22 14:28:07 +0200

    Wisselgeld

---
+++ 04_Variabelen/Avogadro.py
@@ -0,0 +1,18 @@
+# invoer
+
+n_S = float(input('Geef het aantal deeltjes S, uitgedrukt in mol: '))
+
+# berekening
+
+N_A = 6.020 * 10**23
+
+M_S = 32.06
+
+m_S = M_S * n_S
+
+N_S = n_S * N_A
+
+# uitvoer
+
+print(m_S)
+print(N_S)
