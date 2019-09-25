8f8a5b6d0018af57a8daa3a1a9045be9d964bdd7
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-09-25 19:42:10 +0200

    Tijdmeting op Mars

---
+++ 04+Variabelen/Tijdmeting op Mars.py
@@ -0,0 +1,18 @@
+# invoer
+
+aantal_marsdagen = int(input('Geef het aantal Marsdagen: '))
+
+# berekening
+
+tijd_aarde = 88775.244 * aantal_marsdagen
+
+invoer = aantal_marsdagen
+
+aantal_marsdagen_input = tijd_aarde // 86400
+tijd_aarde %= 86400
+
+
+
+# uitvoer
+
+
