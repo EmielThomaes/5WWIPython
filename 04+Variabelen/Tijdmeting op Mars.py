608781211ac02fcc1e18dbe1662d79b9a0a258a1
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-09-26 19:48:27 +0200

    Graansilo vullen

--- 04+Variabelen/Tijdmeting op Mars.py
+++ 04+Variabelen/Tijdmeting op Mars.py
@@ -12,10 +12,13 @@ aantal_aarde_dagen = tijd_aarde_seconden // 86400
 tijd_aarde_seconden %= 86400
 
 aantal_aarde_uren = tijd_aarde_seconden // 2340
-tijd_aarde_seconden %=
+tijd_aarde_seconden %= 2340
 
+aantal_aarde_seconden = int(tijd_aarde_seconden) + 1
 
 
 # uitvoer
 
 print(int(aantal_aarde_dagen))
+print(int(aantal_aarde_uren))
+print(int(aantal_aarde_seconden))
