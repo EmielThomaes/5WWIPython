b7fe61cf6d71cd34bbf5c571a71b0b802466c23d
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-09-26 19:18:54 +0200

    Graansilo vullen

--- 04+Variabelen/Graansilo vullen.py
+++ 04+Variabelen/Graansilo vullen.py
@@ -8,13 +8,14 @@ h = float(input('hoogte graansilo: '))
 
 # berekening
 
-pi = 3.14159
+pi = 3.14159265359
 
 volume_graansilo = (r**2) * pi * h
 hoeveelheid_graan = (c / 10000) * (l * b)
 aantal_graansilo = int((hoeveelheid_graan // volume_graansilo) + 1)
 
-hoogte_laatste_graansilo = hoeveelheid_graan % volume_graansilo
+rest_hoeveelheid_graan = hoeveelheid_graan % volume_graansilo
+hoogte_laatste_graansilo = (rest_hoeveelheid_graan) / ((r**2) * pi)
 
 # uitvoer
 
--- 04+Variabelen/Tijdmeting op Mars.py
+++ 04+Variabelen/Tijdmeting op Mars.py
@@ -4,15 +4,18 @@ aantal_marsdagen = int(input('Geef het aantal Marsdagen: '))
 
 # berekening
 
-tijd_aarde = 88775.244 * aantal_marsdagen
+invoer = int(aantal_marsdagen)
 
-invoer = aantal_marsdagen
+tijd_aarde_seconden = 88775.244 * aantal_marsdagen
 
-aantal_marsdagen_input = tijd_aarde // 86400
-tijd_aarde %= 86400
+aantal_aarde_dagen = tijd_aarde_seconden // 86400
+tijd_aarde_seconden %= 86400
+
+aantal_aarde_uren = tijd_aarde_seconden // 2340
+tijd_aarde_seconden %=
 
 
 
 # uitvoer
 
-
+print(int(aantal_aarde_dagen))
