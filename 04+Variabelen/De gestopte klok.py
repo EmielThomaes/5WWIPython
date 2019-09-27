d6e11d62e47b8b023d18132536aa681540b96266
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-09-27 11:47:38 +0200

    De gestopte klok

---
+++ 04+Variabelen/De gestopte klok.py
@@ -0,0 +1,39 @@
+# invoer
+
+uur1 = int(input("geef uur van vertrek vanaf haar eigen huis: "))
+min1 = int(input("geef min van vertrek vanaf haar eigen huis: "))
+
+uur2 = int(input("geef uur van aankomst bij het huis van haar vriendin: "))
+min2 = int(input("geef min van aankomst bij het huis van haar vriendin: "))
+
+uur3 = int(input("geef uur van vertrek vanaf het huis van haar vriendin: "))
+min3 = int(input("geef min van vertrek vanaf het huis van haar vriendin: "))
+
+uur4 = int(input("geef uur van aankomst bij haar eigen huis: "))
+min4 = int(input("geef min van aankomst bij haar eigen huis: "))
+
+# bereking
+
+y1 = (uur1 * 60) + min1
+y2 = (uur2 * 60) + min2
+y3 = (uur3 * 60) + min3
+y4 = (uur4 * 60) + min4
+
+yy = y4 - y1
+y0 = y3 - y2
+
+vertrek = y4 - y1 + (720 - (720 * (yy / abs(yy))))
+pauze = y3 - y2 + (720 - (720 * (y0 / abs(y0))))
+
+y5 = (vertrek - pauze) / 2
+y6 = y3 + y5
+
+uur_eind = y6 // 60
+min_eind = y6 % 60
+speciaal = uur_eind - 24
+uur_eind -= 12 + (12 * (speciaal / abs(speciaal)))
+
+# uitvoer
+
+print(int(uur_eind))
+print(int(min_eind))
