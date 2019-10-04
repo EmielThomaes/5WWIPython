77412a9e9250798bc51d98968181b49ef6c80aaa
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-10-04 11:17:23 +0200

    test

---
+++ 05_EenvoundigeFuncties/test.py
@@ -0,0 +1,24 @@
+uitvoer = '{} + {} = {}'
+print(uitvoer.format(3, 4, 7))
+print(uitvoer.format(-1, 3, 10))
+
+uitvoer = '{0} + {0} = {1}'
+print(uitvoer.format(3, 6))
+
+uitvoer = '{:d} + {:f} = {:d}'
+print(uitvoer.format(1, 1, 2))
+
+uitvoer = '{0:d} + {0:f} = {1:d}'
+print(uitvoer.format(1, 2))
+
+uitvoer = '{0:d} + {0:.2f} = {1:d}'
+print(uitvoer.format(1, 2))
+
+uitvoer = '{:d} + {:9.9f} = {:d}'
+print(uitvoer.format(1, pow(2, 0.5), 2))
+
+uitvoer = '{:d} + {:9.2f} = {:d}'
+print(uitvoer.format(1, pow(2, 0.5), 2))
+
+uitvoer = '{:d} + {:^9.2f} = {:d}'
+print(uitvoer.format(1, pow(2, 0.5), 2))
