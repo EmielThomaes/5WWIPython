6acd54841060249e981445da6b346181b8603641
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-10-04 11:42:09 +0200

    test

--- 05_EenvoundigeFuncties/test.py
+++ 05_EenvoundigeFuncties/test.py
@@ -22,3 +22,22 @@ print(uitvoer.format(1, pow(2, 0.5), 2))
 
 uitvoer = '{:d} + {:^9.2f} = {:d}'
 print(uitvoer.format(1, pow(2, 0.5), 2))
+
+
+
+from math import sqrt
+print(sqrt(2))
+
+from math import pi
+print(pi)
+
+
+
+import random
+print(random.random())
+
+from random import uniform, random, randint, seed
+seed(1233)
+print(random())
+print(uniform(1, 5))
+print(randint(3, 100))
