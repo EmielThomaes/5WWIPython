78738df164c5528d845fac0d40458d98f2eb2a55
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-10-11 10:47:52 +0200

    Absolute waarde

--- 05_EenvoundigeFuncties/Absolute waarde.py
+++ 05_EenvoundigeFuncties/Absolute waarde.py
@@ -2,8 +2,14 @@
 
 # invoer
 
+x = float(input('x = '))
+y = float(input('y = '))
 
 # berekening
 
+berekening_1 = abs(abs(x) - abs(y))
+berekening_2 = abs(x - y)
 
 # uitvoer
+
+print(str(round(berekening_1, 4)) + ' \N{LESS-THAN OR EQUAL TO} ' + str(round(berekening_2, 4)))
---
+++ 05_EenvoundigeFuncties/test 2.py
@@ -0,0 +1,1 @@
+print('\N{LESS-THAN OR EQUAL TO}')
