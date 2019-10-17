b1ed8a76bcae28c19f2d93e92fba7631f2372c07
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-10-17 21:20:55 +0200

    Sommetjes

--- 05_EenvoundigeFuncties/Sommetjes.py
+++ 05_EenvoundigeFuncties/Sommetjes.py
@@ -12,6 +12,6 @@ uitvoer = '{:>6.0f} + {:<6.0f} = {:<6.0f}'
 
 print(uitvoer.format(a, b, uitkomst))
 print(uitvoer.format(a * 10, b * 10, uitkomst * 10))
-print(uitvoer.format(a * (10**2), b * (10**2), uitkomst * (10**2)))
+print(uitvoer.format(a * pow(10, 2), b * (10**2), uitkomst * (10**2)))
 print(uitvoer.format(a * (10**3), b * (10**3), uitkomst * (10**3)))
 print(uitvoer.format(a * (10**4), b * (10**4), uitkomst * (10**4)))
