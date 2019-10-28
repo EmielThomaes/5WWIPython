2656a82de15867537b6a7ca229e326c971912396
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-10-28 18:38:48 +0100

    Blad steen schaar

--- 06_Condities/Blad steen schaar.py
+++ 06_Condities/Blad steen schaar.py
@@ -25,3 +25,21 @@ elif speler_1 > speler_2:
 # uitvoer
 
 print(uitvoer)
+
+#if ant_1 == ant_2:
+#    uitvoer = 'onbeslist'
+#elif ant_1 == 'blad':
+#    if ant_2 == 'steen':
+#        uitvoer = 'speler 1 wint'
+#    else:
+#        uitvoer = 'speler 2 wint'
+#elif ant_1 == 'steen':
+#    if ant_2 == 'schaar':
+#        uitvoer = 'speler 1 wint'
+#    else:
+#        uitvoer = 'speler 2 wint'
+#else:
+#    if ant_2 == 'blad':
+#        uitvoer = 'speler 1 wint'
+#    else:
+#        uitvoer = 'speler 2 wint'
