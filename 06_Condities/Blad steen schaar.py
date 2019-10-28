22ea29d4ad935d67fb0c8a0be6cc8069a0cb942d
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-10-28 17:54:55 +0100

    Blad steen schaar

---
+++ 06_Condities/Blad steen schaar.py
@@ -0,0 +1,23 @@
+# invoer
+
+speler_1 = input('Speler 1: blad, steen of schaar? ')
+speler_2 = input('Speler 2: blad, steen of schaar? ')
+
+# berekening
+
+if speler_1 == speler_2:
+    uitvoer = 'onbeslist'
+elif speler_1 < speler_2:
+    if speler_1 == 'schaar':
+        uitvoer = 'speler 2 wint'
+    else:
+        uitvoer = 'speler 1 wint'
+elif speler_1 > speler_2:
+    if speler_2 == 'schaar':
+        uitvoer = 'speler 1 wint'
+    else:
+        uitvoer = 'speler 2 wint'
+
+# uitvoer
+
+print(uitvoer)
