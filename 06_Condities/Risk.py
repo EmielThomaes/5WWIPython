fdfb3b9fee0b487021f771d2e40b21a5d72c674d
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-11-07 18:31:08 +0100

    Risk

---
+++ 06_Condities/Risk.py
@@ -0,0 +1,22 @@
+# invoer
+
+dobbelsteen_1_aanvaller = int(input('dobbelsteen 1 aanvaller = '))
+dobbelsteen_2_aanvaller = int(input('dobbelsteen 2 aanvaller = '))
+dobbelsteen_3_aanvaller = int(input('dobbelsteen 3 aanvaller = '))
+
+dobbelsteen_1_verdediger = int(input('dobbelsteen 1 verdediger = '))
+dobbelsteen_2_verdediger = int(input('dobbelsteen 2 verdediger = '))
+
+# berekening
+
+if dobbelsteen_1_verdediger > dobbelsteen_2_verdediger:
+    grootste_ds_verdediger = dobbelsteen_1_verdediger
+    kleinste_ds_verdediger = dobbelsteen_2_verdediger
+else:
+    grootste_ds_verdediger = dobbelsteen_2_verdediger
+    kleinste_ds_verdediger = dobbelsteen_1_verdediger
+
+if dobbelsteen_1_aanvaller > dobbelsteen_2_aanvaller and dobbels
+
+# uitvoer
+
