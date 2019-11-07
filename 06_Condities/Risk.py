ff3c3235d4e979f031cc8f123b17d280a890bcfc
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-11-07 19:43:21 +0100

    Risk

--- 06_Condities/Risk.py
+++ 06_Condities/Risk.py
@@ -9,7 +9,14 @@ dobbelsteen_2_verdediger = int(input('dobbelsteen 2 verdediger = '))
 
 # berekening
 
+grootste_ds_aanvaller = max(dobbelsteen_1_aanvaller,dobbelsteen_2_aanvaller,dobbelsteen_3_aanvaller)
+kleinste_ds_aanvaller = min(dobbelsteen_1_aanvaller,dobbelsteen_2_aanvaller,dobbelsteen_3_aanvaller)
+middelste_ds_aanvaller = (dobbelsteen_1_aanvaller + dobbelsteen_2_aanvaller + dobbelsteen_3_aanvaller) - (grootste_ds_aanvaller + kleinste_ds_aanvaller)
 
+grootste_ds_verdediger = max (dobbelsteen_1_verdediger,dobbelsteen_2_verdediger)
+kleinste_ds_verdediger = min(dobbelsteen_1_verdediger,dobbelsteen_2_verdediger)
+
+if grootste_ds_aanvaller > grootste_ds_verdediger:
 
 # uitvoer
 
