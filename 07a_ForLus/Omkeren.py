3ce21d8ad97452262c060a4bcbd0c5170ed9d679
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-11-08 12:00:31 +0100

    Veelvouden

---
+++ 07a_ForLus/Omkeren.py
@@ -0,0 +1,14 @@
+# invoer
+
+woord = input('Geef een woord: ')
+
+# berekening
+
+omgekeerd_woord = ''
+
+for letter in woord:
+    omgekeerd_woord = letter + omgekeerd_woord
+
+# uitvoer
+
+print(omgekeerd_woord)
---
+++ 07a_ForLus/Veelvouden.py
@@ -0,0 +1,16 @@
+# invoer
+
+getal = int(input('geef een getal: '))
+
+# berekening
+
+uitkomst = 0
+
+for veelvoud in range(getal, 100, getal):
+    uitkomst += veelvoud
+
+# uitvoer
+
+print(uitkomst)
+
+
