46f42bc13aa62d7f08f95dd0725904d8b8448ce0
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-11-11 19:49:47 +0100

    De diatomist

--- 05+EenvoudigeFuncties/De diatomist.py
+++ 05+EenvoudigeFuncties/De diatomist.py
@@ -9,9 +9,12 @@ grote_straal = float(input('Straal van de grote cirkel: '))
 
 maximum_aantal_kleine_cirkels = int((0.83 * ((grote_straal ** 2) / (kleine_straal ** 2))) - 1.9)
 
-bedekkingsgraad =
+oppervlakte_grote_cirkel = (pow(grote_straal, 2) * pi)
+oppervlakte_kleine_cirkel = (pow(kleine_straal, 2) * pi)
 
-uitvoer = 'test {:d} + {:.2f}'
+bedekkingsgraad = ((maximum_aantal_kleine_cirkels * oppervlakte_kleine_cirkel) / oppervlakte_grote_cirkel) * 100
+
+uitvoer = '{:d} kleine cirkels bedekken {:.2f}% van de grote cirkel'
 
 # uitvoer
 
