6d8bb8961ea42cc61fcbc6dc548723c9824d6de2
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2019-11-22 11:12:21 +0100

    Grootste en gemiddelde

--- 07a_ForLus/Grootste en gemiddelde.py
+++ 07a_ForLus/Grootste en gemiddelde.py
@@ -4,13 +4,21 @@ aantal_getallen = int(input('aantal getallen = '))
 
 # berekening
 
-gemiddelde = 0
+# lees het eerste getal voor de lus in
+getal_0 = int(input('geef getal = '))
 
-for i in range(aantal_getallen):
-    gemiddelde = int(input('getal = ')) / aantal_getallen
+# het eerste getal is onmiddellijk de som en het grootste getal
+som, grootste = getal_0, getal_0
+
+for i in range(aantal_getallen - 1):
+    getal = int(input('geef getal = '))
+    som += getal
+    grootste = max(grootste, getal)
+
+gemiddelde = som / aantal_getallen
 
 uitvoer = 'Het grootste getal is {} en het gemiddelde is {:.2f}'
 
 # uitvoer
 
-print(uitvoer.format(gemiddelde, gemiddelde))
+print(uitvoer.format(grootste, gemiddelde))
