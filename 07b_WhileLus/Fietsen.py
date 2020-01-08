b3935f5cd42275292e546bae1a4fc96b78bcbb8a
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-01-08 20:41:01 +0100

    Fietsen

---
+++ 07b_WhileLus/Fietsen.py
@@ -0,0 +1,13 @@
+# invoer
+
+snelheid_stijn = int(input('geef de snelheid van Stijn (in m/s): '))
+snelheid_kaat = int(input('geef de snelheid van Kaat (in m/s): '))
+afstand = int(input('geef de afstand tussen de twee huizen (in m): '))
+
+# berekening
+
+afstand_stijn = 0
+afstand_kaat = 0
+
+while (afstand_stijn + afstand_kaat) <= afstand:
+
