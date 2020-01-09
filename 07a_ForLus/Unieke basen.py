42f21470fcf38032ca82e053f820c6094fbca691
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-01-09 18:14:02 +0100

    Unieke basen

--- 07a_ForLus/Unieke basen.py
+++ 07a_ForLus/Unieke basen.py
@@ -4,8 +4,22 @@ aantal_basen = int(input('aantal basen = '))
 
 # berekening
 
+dna_a = ''
+dna_t = ''
+dna_g = ''
+dna_c = ''
+
 for i in range(aantal_basen):
-    i = input('base = ')
-    if i
+    soort = input('geef de soort DNA: ')
+    if soort == 'A':
+        dna_a = ' A'
+    if soort == 'T':
+        dna_t = ' T'
+    if soort == 'G':
+        dna_g = ' G'
+    if soort == 'C':
+        dna_c = ' C'
+
+aantal_verschillende_soorten = int((len(str(dna_a + dna_t + dna_g + dna_c))) / 2)
 
 # uitvoer
