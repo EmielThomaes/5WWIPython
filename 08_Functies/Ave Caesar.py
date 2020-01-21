a2afcc88a396fbd9918d2f2f16229947add46dd6
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-01-21 09:08:31 +0100

    Ave Caesar

--- 08_Functies/Ave Caesar.py
+++ 08_Functies/Ave Caesar.py
@@ -2,5 +2,10 @@ def is_letter(t):
     return ord('a') <= ord(t) <= ord('z') or ord('A') <= ord(t) <= ord('Z')
 
 
-def roteer_letter(l, aantal_plaatsen):
-    return chr(ord(l) + aantal_plaatsen)
+def roteer_letter(letter, aantal_plaatsen):
+    if ord(letter) + aantal_plaatsen > 122:
+        aantal_plaatsen = (ord(letter) + aantal_plaatsen) - 122
+        nieuwe_letter = chr(ord('a') + aantal_plaatsen - 1)
+    return nieuwe_letter
+
+print(roteer_letter('z', 22))
