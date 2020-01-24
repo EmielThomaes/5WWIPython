cae3fdb05e1c3c9c20951594debe9c945ad4143c
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-01-24 11:56:01 +0100

    Germaniseer

--- 10_Strings/Germanisering.py
+++ 10_Strings/Germanisering.py
@@ -1,2 +1,15 @@
-def germaniseer(zin):
+# overloop alle letters van de strin
+# indien de letter een spatie is
+# volgende letter wordt hoofdletter
+# werk met: nieuwe_zin +=
 
+def germaniseer(zin):
+    nieuwe_zin = ''
+    for i in range (0, len(zin)):
+        if zin[i] == ' ':
+            nieuwe_zin += zin[i+1].upper()
+        else:
+            nieuwe_zin += zin[i]
+    return nieuwe_zin
+
+print(germaniseer('Dit is een test'))
