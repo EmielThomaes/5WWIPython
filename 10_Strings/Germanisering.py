81bdd5a2bdc227a6a869eb7e0b0361547a2ebcef
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-01-24 12:00:26 +0100

    Germaniseer

--- 10_Strings/Germanisering.py
+++ 10_Strings/Germanisering.py
@@ -8,7 +8,7 @@ def germaniseer(zin):
     for i in range (0, len(zin)):
         if zin[i] == ' ':
             nieuwe_zin += ' ' + zin[i+1].upper()
-        else:
+        elif zin[i - 1] !=  ' ':
             nieuwe_zin += zin[i]
     return nieuwe_zin
 
