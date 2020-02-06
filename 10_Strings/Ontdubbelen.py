284c72a3a6996aa3595398fff1ac078b68c9a4f6
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-02-06 21:26:37 +0100

    Geen twee na elkaar

--- 10_Strings/Ontdubbelen.py
+++ 10_Strings/Ontdubbelen.py
@@ -1,1 +1,8 @@
 def ontdubbelen(woord):
+    nieuw_woord = ''
+    vorige_letter = ''
+    for i in range(len(woord)):
+        if woord[i] != vorige_letter:
+            nieuw_woord += woord[i]
+        vorige_letter = woord[i]
+    return nieuw_woord
