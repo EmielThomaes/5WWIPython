c4e5da587eb1d45c05518481c199b1c2890a939f
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-02-06 19:12:47 +0100

    Vermoeden van Collatz

--- 08_Functies/Vermoeden van Collatz.py
+++ 08_Functies/Vermoeden van Collatz.py
@@ -1,3 +1,13 @@
-# if
+def volgend_collatz_getal(n):
+    if n % 2 == 0:
+        n /= 2
+    else:
+        n = (n * 3) + 1
+    return int(n)
 
-# while
+def collatz(n):
+    cyclelengte = 1
+    while n != 1:
+        n = volgend_collatz_getal(n)
+        cyclelengte += 1
+    return cyclelengte
