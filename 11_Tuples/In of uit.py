3f88254d983f81a19614d614fae5d2dc403ee86c
EmielThomaes <emiel.thomaes@sgsintpaulus.eu>
2020-02-14 11:15:02 +0100

    In of uit?

--- 11_Tuples/In of uit.py
+++ 11_Tuples/In of uit.py
@@ -1,2 +1,15 @@
-def binnen_of_buiten(co_middelpunt, co_punt_cirkel, co_willekeurig_punt):
+from math import sqrt
 
+def binnen_of_buiten(middelpunt, punt_cirkel, willekeurig_punt):
+
+    straal = sqrt((punt_cirkel[0] - middelpunt[0]) ** 2 + (punt_cirkel[1] - middelpunt[1]) ** 2)
+    afstand_middelpunt = sqrt((willekeurig_punt[0] - middelpunt[0]) ** 2 + (willekeurig_punt[1] - middelpunt[1]) ** 2)
+
+    if afstand_middelpunt > straal:
+        plaats = 'buiten'
+    elif afstand_middelpunt < straal:
+        plaats = 'binnen'
+    else:
+        plaats = 'op'
+
+    return plaats, round(afstand_middelpunt, 4)
